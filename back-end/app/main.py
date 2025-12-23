8279e9cc907fe5d064bab0ba27c66de65d353877
Pieter Gilissen <pieter_gilissen@hotmail.com>
2025-12-23 19:44:19 +0100

    fix

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -17,12 +17,6 @@ app = FastAPI(title="Baby Page API")
 #     allow_headers=["*"]
 # )
 
-app.mount(
-    "/static",
-    StaticFiles(directory="app/static"),
-    name="static"
-)
-
 def get_db():
     db = SessionLocal()
     try:
@@ -30,25 +24,31 @@ def get_db():
     finally:
         db.close()
 
-@app.get("api/measurement", response_model=list[schemas.MeasurementResponse])
+@app.get("/api/measurement", response_model=list[schemas.MeasurementResponse])
 def get_measurements(db: Session = Depends(get_db)):
     return crud.get_all_measurements(db)
 
-@app.post("api/measurement", response_model=schemas.MeasurementResponse)
+@app.post("/api/measurement", response_model=schemas.MeasurementResponse)
 def add_measurement(measurement: schemas.MeasurementCreate, db: Session = Depends(get_db)):
     return crud.create_measurement(db, measurement)
 
-@app.get("api/board-card", response_model=list[schemas.BoardCardResponse])
+@app.get("/api/board-card", response_model=list[schemas.BoardCardResponse])
 def get_board_cards(db: Session = Depends(get_db)):
     return crud.get_all_board_cards(db)
 
-@app.post("api/board-card", response_model=schemas.BoardCardResponse)
+@app.post("/api/board-card", response_model=schemas.BoardCardResponse)
 def create_board_card(card: schemas.BoardCardCreate, db: Session = Depends(get_db)):
     return crud.create_board_card(db, card)
 
-@app.put("api/board-card", response_model=schemas.BoardCardResponse)
+@app.put("/api/board-card", response_model=schemas.BoardCardResponse)
 def update_board_card(card: schemas.BoardCardUpdate, db: Session = Depends(get_db)):
     return crud.update_board_card(db, card)
+
+app.mount(
+    "/static",
+    StaticFiles(directory="app/static"),
+    name="static"
+)
 
 @app.get("/")
 def serve_spa():
